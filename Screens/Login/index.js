c47b5697bcf456d1d89746b4b8e5f4a7560fb89e
ManalLiaquat <m.manalliaquat@gmail.com>
2018-12-17 23:41:34 +0500

    initializing firebase

--- Screens/Login/index.js
+++ Screens/Login/index.js
@@ -1,41 +1,59 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button, AsyncStorage, Alert } from 'react-native';
-import { Facebook } from 'expo';
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  AsyncStorage,
+  Alert
+} from "react-native";
+import { Facebook } from "expo";
+import firebase from "../../Config/Firebase";
 
 export default class Login extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      type: 'cancel',
-      token: null,
+      type: "cancel",
+      token: null
     };
     this.logIn = this.logIn.bind(this);
   }
 
   componentDidMount() {
-    // setTimeout(() => {
-    //   this.logIn();
-    // }, 2000);
+    setTimeout(() => {
+      this.logIn();
+    }, 1500);
   }
 
   async logIn() {
     const { type, token } = await Facebook.logInWithReadPermissionsAsync(
-      '1302749559866771',
+      "1302749559866771",
       {
-        permissions: ['public_profile'],
+        permissions: ["public_profile"]
       }
     );
-    if (type === 'success') {
+    if (type === "success") {
       const response = await fetch(
         `https://graph.facebook.com/me?access_token=${token}`
       );
-      Alert.alert(`Logged in! Hi ${(await response.json()).name}!`);
-      console.log(await response.json());
+      let res = await response.json();
+      // Alert.alert(`Logged in! Hi ${res.name}!`);
+      // console.log(res);
       // console.log('Token', token);
-      await AsyncStorage.setItem('userToken', token);
-      // this.props.navigation.navigate('Home');
+      await AsyncStorage.setItem("userToken", token);
+      this.props.navigation.navigate("Home");
+      firebase
+        .database()
+        .ref(`/users/`)
+        .set(res);
+      firebase
+        .database()
+        .ref("/fcmTokens")
+        .child(token)
+        .set(res.uid);
     } else {
-      console.log('type === cancel');
+      console.log("type === cancel");
       // type === 'cancel'
     }
   }
